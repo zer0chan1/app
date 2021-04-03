f710493154f216b765acc166908d99b95e2ea0cf
zer0chan1 <multibridge@protonmail.com>
2021-04-03 12:03:08 +0200

    Site live

---
+++ precache-manifest.90d65c72aa7fd25fe0f809c79f2c098c.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cffc2b17f321b78cde92e00b7aa8c1a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04fee63e214f50ffe873",
+    "url": "/static/css/main.0651d610.chunk.css"
+  },
+  {
+    "revision": "b96b12792a840c86a6d9",
+    "url": "/static/js/0.2b406d2d.chunk.js"
+  },
+  {
+    "revision": "7fd7eb181a494a0b3395",
+    "url": "/static/js/10.a0e09b8c.chunk.js"
+  },
+  {
+    "revision": "2cb22a64ce82c60ab9ff",
+    "url": "/static/js/3.2291b7f4.chunk.js"
+  },
+  {
+    "revision": "1e03de31d2136e882aace83de0c35ca0",
+    "url": "/static/js/3.2291b7f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5ea612cc0f9f56ee40d",
+    "url": "/static/js/4.cd20d413.chunk.js"
+  },
+  {
+    "revision": "f0cec197385dd35d7bff",
+    "url": "/static/js/5.3da977c5.chunk.js"
+  },
+  {
+    "revision": "3bad4771553c18618592",
+    "url": "/static/js/6.8c7e8027.chunk.js"
+  },
+  {
+    "revision": "800c8c88b71490b35d9e636bbf8c583d",
+    "url": "/static/js/6.8c7e8027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "677b108bdd8c2529f617",
+    "url": "/static/js/7.89cb9065.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "/static/js/7.89cb9065.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87ae919a4338f51f4e2c",
+    "url": "/static/js/8.ae07bbd4.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/8.ae07bbd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b688734ec1b141d3157c",
+    "url": "/static/js/9.614aa863.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "/static/js/9.614aa863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fee63e214f50ffe873",
+    "url": "/static/js/main.3ba36d54.chunk.js"
+  },
+  {
+    "revision": "4ae7fde4a926ea29fe17",
+    "url": "/static/js/runtime-main.1d74c544.js"
+  },
+  {
+    "revision": "c6eba6d3dc7f7b29a54c38a64cb7d2bc",
+    "url": "/static/media/icon-view.c6eba6d3.svg"
+  },
+  {
+    "revision": "32a548a5d774658c473dd65f5e143b29",
+    "url": "/static/media/logo1.32a548a5.png"
+  },
+  {
+    "revision": "156664c9217e8a4a677fcc28f55f43ca",
+    "url": "/static/media/menu.156664c9.svg"
+  },
+  {
+    "revision": "c06f3a3e804ebc7343949fdca3fdd7f8",
+    "url": "/static/media/metamask-fox.c06f3a3e.svg"
+  },
+  {
+    "revision": "7b3cd1265e44c435d11d5bd92aff2f57",
+    "url": "/static/media/token-gold.7b3cd126.png"
+  },
+  {
+    "revision": "edcc1ab5dde5cb3d5cf134c4aade641b",
+    "url": "/static/media/trustwallet.edcc1ab5.png"
+  },
+  {
+    "revision": "d1f736120a29462726fb25a8e88cffba",
+    "url": "/static/media/wallet-connect.d1f73612.svg"
+  },
+  {
+    "revision": "044830ee13f3abd9e4ed3ef7b249296b",
+    "url": "/static/media/walletc2.044830ee.svg"
+  },
+  {
+    "revision": "5928af5a120b5a8b7607d90067c43b2a",
+    "url": "/static/media/walletc4.5928af5a.svg"
+  }
+]);
